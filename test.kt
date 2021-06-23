78d1fe235b7801b8c61aa46d7cac92b839617a28
Jose Ricardo Citerio Alcala <r1c4rd0.10100@gmail.com>
2021-06-23 18:34:19 -0400

    Serie2

--- test.kt
+++ test.kt
@@ -7,7 +7,8 @@
     
 fun main() {
 	
-    val result = calculateSerie1(10, 4)
+    //val result = calculateSerie1(10, 4)
+    val result = calculateSerie2(1341)
     println("Result $result")
     
 }
@@ -24,6 +25,31 @@ fun calculateSerie1(x: Int, y: Int): Int{
     while(i > 0){
         sum = sum * times
         times++
+        i--
+    }
+    
+    return sum
+    
+}
+
+fun calculateSerie2(x: Int): Int{
+    
+    //root
+    var i = x 
+    var sum = 1
+    var root = 1
+    var n = 1
+    //var times = 2
+    //sum 1
+    //n 2
+    //root 1
+    //i 2
+    
+    while(i > 0){
+        
+        root = (2 * n) - 1
+        sum = sum + root
+        n++
         i--
     }
     
